507f725aec987acbaaa053c5e5286ff8e72391fd
AyushSingh916 <ayushsingh916924@gmail.com>
2024-06-16 10:36:42 +0530

    adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Metadata } from 'next';
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({
   children,
@@ -12,6 +13,7 @@ export default function RootLayout({
     <html lang="en" className='scroll-smooth'>
       <body>
         <Header />
+        <Analytics />
         {children}
         <Footer />
       </body>
