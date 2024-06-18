8223e29d74327b0c435ede3e2be5573789385ab8
AyushSingh916 <ayushsingh916924@gmail.com>
2024-06-19 00:40:18 +0530

    addinf speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,18 @@ import { Metadata } from 'next';
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className='scroll-smooth'>
+    <html lang="en" className="scroll-smooth">
       <body>
         <Header />
         <Analytics />
+        <SpeedInsights />
         {children}
         <Footer />
       </body>
